0e5cdb6c4757c693a93eab1e12165d26551734bd
rrequero <rrequero@gmail.com>
2017-11-16 16:36:44 +0100

    update query

--- app/src/services/neo4j.service.js
+++ app/src/services/neo4j.service.js
@@ -117,7 +117,7 @@ RETURN DISTINCT datasets
 const QUERY_GET_LIST_CONCEPTS = `
 MATCH (c:CONCEPT)
 OPTIONAL MATCH (c)<-[:TAGGED_WITH]-(d:DATASET)
-RETURN c.id, c.label, c.synonyms, labels(c) as labels, count(d) as number_of_datasets_tagged
+RETURN c.id, c.label, c.synonyms, labels(c) as labels, count(d) as number_of_datasets_tagged, d.id AS datasets
 `;
 
 const QUERY_GET_CONCEPTS_INFERRED_FROM_LIST = `
