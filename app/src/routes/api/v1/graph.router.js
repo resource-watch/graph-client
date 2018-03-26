717f728aeb71e81f09e28a9d90ca8916f2f7d739
Sergio Gordillo <sgordillogallardo@gmail.com>
2018-03-26 13:10:28 +0200

    fix filter without concepts

--- app/src/routes/api/v1/graph.router.js
+++ app/src/routes/api/v1/graph.router.js
@@ -219,6 +219,8 @@ class GraphRouter {
     }
     if (datasetIds && datasetIds.length > 0 && sort && (sort.includes('most-viewed') || sort.includes('most-favorited'))) {
       datasetIds = await neo4jService.sortDatasets(sort, datasetIds);
+    } else if (!datasetIds){
+      datasetIds = await neo4jService.sortDatasets(sort, []);
     }
     ctx.body = {
       data: datasetIds
